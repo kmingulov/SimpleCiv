3a6ff88cf51ec402f9e8195aa468feb385330c1b
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2012-04-29 16:16:46 +0700

    Rewrote test for game model.

--- tests/model/test.c
+++ tests/model/test.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "../../game/world/world.h"
+#include "../../modules/parser/xml.h"
 
 int main()
 {
-    // In future we will parse it all from config file.
-    WorldProperties * properties = malloc(sizeof(WorldProperties));
-    properties -> map_w = 10;
-    properties -> map_h = 10;
-    properties -> players_count = 5;
-    properties -> player_names = daCreate();
-
-    for(int i = 0; i < 5; i++)
-    {
-        char * name = malloc(sizeof(char) * 8);
-        name[0] = 'P'; name[1] = 'l'; name[2] = 'a'; name[3] = 'y'; name[4] = 'e'; name[5] = 'r';
-        name[6] = 49 + i; name[7] = '\0';
-        daPrepend(properties -> player_names, name);
-    }
+    // Parse settings from config file.
+    WorldProperties * properties = parseConfig("../../config.xml");
 
     // Creating world.
     World * world = createWorld(properties);
