788cc2b1c9909305265c994d2cd5db6fe84fb01c
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2012-05-05 00:48:20 +0700

    Beautify test/model/test.c

--- tests/model/test.c
+++ tests/model/test.c
@@ -2,36 +2,15 @@
 #include <stdlib.h>
 
 #include "../../game/world/world.h"
-#include "../../modules/parser/string_functions.h"
-
-#include "../../modules/parser/xml.h"
-#include "../../modules/technology/technology.h"
-
 #include "../../modules/view/view.h"
-
-
-
-void techForeachNeighbour(Node * parent, Node * child, Edge * link)
-{
-    Technology * t1 = (Technology *) parent -> data;
-    Technology * t2 = (Technology *) child -> data;
-    printf("%s ---%d--> %s\n", t1 -> name, link -> type, t2 -> name);
-}
 
 int main()
 {
-    // Creating world.
+    // Creating world and view.
     World * world = createWorld();
-
-    if(world == NULL)
-    {
-        printf("An error has occurred. Terminating.\n");
-        return 1;
-    }
-
     View * view = createView();
 
-    if(view == NULL)
+    if(world == NULL || view == NULL)
     {
         printf("An error has occurred. Terminating.\n");
         return 1;
@@ -40,17 +19,7 @@ int main()
     //~ drawInterface(&view->iface);
     //~ drawMap(world->graph_map, &view->iface);
 
-
-    /*foreachNeighbour(world -> techs_tree, &techForeachNeighbour);
-    printf("\n");
-
-    foreachNeighbour(((TechnologyCommonInfo *) world -> techs_info -> data[0]) -> tech_in_tree, &techForeachNeighbour);
-    printf("\n");
-
-    foreachNeighbour(((TechnologyCommonInfo *) world -> techs_info -> data[1]) -> tech_in_tree, &techForeachNeighbour);
-    printf("\n");*/
-
-    // Destroing world.
+    // Destroing everything.
     destroyWorld(world);
     destroyView(view);
 
